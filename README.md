e409efa71bc2eb373a93120424debd51c79be570
gamze <gamzekandara@gmail.com>
2023-12-25 12:33:10 -0500

    update index.js

--- README.md
+++ README.md
@@ -1,5 +1,35 @@
-description of the app
+Welcome to my Phase-1 project!
 
-url of the repo
+INTRODUCTION 
 
-how to run it
+This app is a single-page, responsive website using HTML, CSS, and JavaScript.
+It allows user to quickly add and display products on the webpage.With this app users can effortlessly manage their inventory, helps them keep track of their products and make updates. The app creates a card for each product and its price. 'Add' and 'Clear' buttons simply allow user to create a space for new product and hides them all once 'Clear' button clicked.
+
+'Online Marketplace' app is a good start for the ones who want to build their own E-commerce website.'Add to Cart' button is not activated but  added in JavaScript as a demonstration, for users to easily click and add the items they would like to buy!
+
+Here is the URL of the API 'https://dummyjson.com/products' which includes 30 different products, brand names, and images. These are also accessible in db.json file.
+
+#To install JSON Server : 
+
+npm install -g json-server 
+
+
+#To start the server :
+
+json-server --watch db.json
+
+
+If you are a developer and want to contribute, follow the instructions below: 
+
+INSTRUCTIONS 
+
+1.Fork this repository
+2.Clone the forked repository in your local machine ( git clone)
+3.Add your contributions(code or documentation) 
+4.Commit and push ( git commit / git push)
+
+Here is link for pull requests : https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/working-with-forks/fork-a-repo 
+
+I hope you enjoy and I look forward to your contributions. 
+
+
--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-// contentloads and displays the products
-// last minute deals change color
-// submit event for form, that enables search by brand name
-
-
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  let p = document.querySelector('p')
-  let ul = document.getElementById('show-details')
-  let form = document.getElementById('product-form')
-  let main = document.getElementById('main')
+let p = document.querySelector('p')
+let ul = document.getElementById('show-details')
+let form = document.getElementById('product-form')
+let main = document.getElementById('main')
 
 
-  fetch('https://dummyjson.com/products') // // fetch API
+  fetch('https://dummyjson.com/products')
   .then(response => response.json())
   .then((data) => { 
     
@@ -21,8 +16,6 @@ document.addEventListener('DOMContentLoaded', () => {
     // console.log(data)
     
     renderProduct(productArr);
-    // handleAdd(e.target.pname.value)
-    // handleAdd(e.target.category.value)
     
   })
   .catch((error) => console.error('Error fetching products:', error))
@@ -32,16 +25,15 @@ document.addEventListener('DOMContentLoaded', () => {
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     let userInput = e.target.pname.value
-    // console.log(e.target.pname.value)
-   let catValue = e.target.category.value
+    let catValue = e.target.category.value
+
     handleAdd(userInput, catValue)
     
     form.reset();
-  
-  }) // add eventListener to the form;
+  }) 
 
   function handleAdd(name, price){
-    //create DOM element , then append
+  
     let card2 = document.createElement('div');
     card2.className = 'card';
   
@@ -51,21 +43,15 @@ document.addEventListener('DOMContentLoaded', () => {
     let price2 = document.createElement('h4');
     price2.textContent = `$${price}`
 
-
-
     card2.appendChild(model)
     card2.appendChild(price2)
     main.appendChild(card2)
     
-
   }
 
-  function renderProduct(productArr) { // fetch API
-   
-// use forEach to loop over and display product, create tags for each 
+  function renderProduct(productArr) { 
 
-   let displays =  productArr.map((productObj) => { // create li for name(title) , price
-  let card = document.createElement('div');
+   let displays =  productArr.map((productObj) => { 
   card.className = 'card';
 
   let name = document.createElement('h2');
@@ -85,23 +71,19 @@ document.addEventListener('DOMContentLoaded', () => {
 }
 
 
- // add click event to the 'clear' button', and clear out innerHTML in dom 
 let btn = document.getElementById('clear')
 btn.addEventListener('click', () => {
- main.innerHTML = ''; // Clear the innerHTML 
+ main.innerHTML = ''; 
 });
-   // add mouseover event to change the color of 'Last Minute Deals' 
+   
 p.addEventListener('mouseover', () => {
   p.style.color = "red"
 
- }); 
+}); 
 
-   // Reset color on mouseout
-  p.addEventListener('mouseout', () => {
-  p.style.color = "rebeccapurple"; 
-  });
-
-
+p.addEventListener('mouseout', () => {
+p.style.color = "rebeccapurple"; 
+});
 
 });
 
