a5125f55a39c2ff8a915d39a6b2be86a3a7570d2
gamze <gamzekandara@gmail.com>
2023-12-24 20:09:53 -0500

    index.js commit3

--- index.js
+++ index.js
@@ -1,88 +1,118 @@
 
 
 // use mouseover event to get the title color's changed to red
-// add submit event to form to save data in JSON
-// keydown event.
-// filter for apple products
+// add submit event 
+// filter for apple products , click ?
+// clear btn to clear DOM
+// check call stack
 
 
 document.addEventListener('DOMContentLoaded', () => {
 
+  let p = document.querySelector('p')
+  let ul = document.getElementById('show-details')
+  let form = document.getElementById('product-form')
+  let main = document.getElementById('main')
   // Define the number variable if needed
   // let number = 1;
-//  show the first 5 products' name once user enters 'phone' last minute deals'
-  function fetchProduct() { // fetch API
-    // Grab show-details
-    // debugger;
-// use forEach to loop over and show images for each product, create tags for each 
-    fetch('https://dummyjson.com/products') // fetch products
-      .then(response => response.json())
-      .then((data) => { 
-        console.log(data);
-        let productArr = data.products;
-        console.log(productArr)// products array
 
-         productArr.forEach((productObj) => { // create li for name(title) , price
-            let name = document.createElement('h2');
-            name.textContent = productObj.title;
 
-            let price = document.createElement('h4');
-            price.textContent = `$${productObj.price}`;
 
-            ul.appendChild(name);
-            ul.appendChild(price);
-    
-          });
-        
-      })
-      .catch((error) => console.error('Error fetching products:', error));
+  fetch('https://dummyjson.com/products') // // fetch API
+  .then(response => response.json())
+  .then((data) => { 
+    console.log(data);
+    let productArr = data.products;
+    // renderProduct(productArr)// products array
+    renderProduct(productArr)
+  })
+  .catch((error) => console.error('Error fetching products:', error))
+
+
+      // function filters for Apple and displays on webpage
+      function filterProduct(productArr) {
+        let productName = e.target.pname.value
+        let brand = data.products.brand
+        ul.textContent = "";
       
-  };
- // add mouseover event to change the color of 'Last Minute Deals' 
- fetchProduct(); // call fetch product func async 
- let ul = document.getElementById('show-details');
+        let filtered = productArr.filter((productObj) => productObj.startsWith(brand));
+        
+      
+        renderProduct(filtered)
+      
+      }
+      
+       // add mouseover event to change the color of 'Last Minute Deals' 
+       // call fetch product func async 
+       
+       
+        form.addEventListener('submit', (e) => {  // add eventListener to the form
+        e.preventDefault();
+        console.log(e);
+        let productName = e.target.pname.value
+        let category = e.target.category.value
+        let productArr = data.products;
+        filterProduct(productArr);
+      
+         form.reset();
+       });
 
- let form = document.getElementById('product-form')
- form.addEventListener('submit', (e) => {  // add eventListener to the form
-   e.preventDefault();
-   console.log(e);
-   let obj = {
-    productName: e.target.pname.value,
-    id: e.target.category.value,
-  }
- 
-  
-   addProduct(obj);
-   form.reset();
- });
 
 
- let p = document.querySelector('p');
- p.addEventListener('mouseover' , () => {
+
+  function renderProduct(productArr) { // fetch API
+    // debugger;
+// use forEach to loop over and display product, create tags for each 
+ productArr.forEach((productObj) => { // create li for name(title) , price
+  let card = document.createElement('div');
+  card.className = 'card';
+
+  let name = document.createElement('h2');
+  name.textContent = productObj.title;
+
+  let price = document.createElement('h4');
+  price.textContent = `$${productObj.price}`;
+
+  main.appendChild(card)
+  card.appendChild(name);
+  card.appendChild(price);
+    
+  });
+        
+}
+
+
+
+ p.addEventListener('mouseover', () => {
   p.style.color = "red"
 
  }); 
 
    // Reset color on mouseout
   p.addEventListener('mouseout', () => {
-  p.style.color = ''; // This will remove the inline style and use the CSS defined color
+  p.style.color = "blue"; 
   });
 
 
-// Save product in JSON
-function addProduct(obj) { //  error 404
-  // post Method
-  fetch('https://dummyjson.com/products', {  
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(obj)
+ // add click event to the 'clear' button', and clear out innerHTML in dom 
 
-  })
-    .then(resp => resp.json())
-    .then(obj => console.log(obj));
-}
+ let btn = document.getElementById('clear')
+ btn.addEventListener('click', () => {
+  main.innerHTML = ''; // Clear the innerHTML 
+});
+
+// function renderImg (urlArray) {
+//   // forEach  to get each url from the array
  
-  
+//     let divForImg = document.getElementById('dog-image-container')
+//      urlArray.forEach(url => {
+//          let imgTag = document.createElement('img');
+//          imgTag.src = url
+        
+//          divForImg.append(imgTag)
+//      });
+//    }
+ 
+
+
 });
